3e09a2aa37aa2083e7fd0a68c34eb9379c6c63f6
AntonLueders <145451222+AntonLueders@users.noreply.github.com>
2026-07-13 10:21:00 +0200

    Update printdata.c

--- Stadia/printdata.c
+++ Stadia/printdata.c
@@ -10,11 +10,11 @@ void PrintOvito(FILE *f, Particle *P, int step, double L) {
 	for (int i = 0; i < N; i++) {	
 	
 		double phi = atan2(P[i].e[1], P[i].e[0]);
-// Kombinierter Quaternion nach Rotation um 90 Grad um die Y-Achse und dann um 90 Grad um die Z-Achse
-double w =  0.5 * sqrt(1. + cos(phi));
-double x = sin(phi) / (4.*w);
-double y = -(cos(phi)+1.)/(4.*w);
-double z =  sin(phi)/(4.*w);
+		// Kombinierter Quaternion nach Rotation um 90 Grad um die Y-Achse und dann um 90 Grad um die Z-Achse
+		double w =  0.5 * sqrt(1. + cos(phi));
+		double x = sin(phi) / (4.*w);
+		double y = -(cos(phi)+1.)/(4.*w);
+		double z =  sin(phi)/(4.*w);
 
 		fprintf(f, "s1\t%f\t%f", P[i].r[0], P[i].r[1]);
 		fprintf(f, "\t%f\t%f\t%f\t%f", w,x,y,z);
