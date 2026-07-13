faeae35c857f711f60c275b86a89e54cb0ac8b6a
AntonLueders <145451222+AntonLueders@users.noreply.github.com>
2026-07-13 23:12:35 +0200

    Update printdata.c

--- Lenses/printdata.c
+++ Lenses/printdata.c
@@ -7,20 +7,6 @@ void PrintOvito(FILE *f, Particle *P, int step, double L) {
     fprintf(f, "%d\n", N);
     fprintf(f, "step = %d\n", step + 1);
 
-	/*for (int i = 0; i < N; i++) {	
-	
-		double phi = atan2(P[i].e[1], P[i].e[0]);
-
-		double w =  0.5 * sqrt(1. + cos(phi));
-		double x = sin(phi) / (4.*w);
-		double y = -(cos(phi)+1.)/(4.*w);
-		double z =  sin(phi)/(4.*w);
-
-		fprintf(f, "s1\t%f\t%f", P[i].r[0], P[i].r[1]);
-		fprintf(f, "\t%f\t%f\t%f\t%f", w,x,y,z);
-		fprintf(f, "\t%f\t%f\t%f\t", P[i].a, P[i].b, P[i].b);
-		fprintf(f, "%f\n", L);
-	}*/
 	for (int i = 0; i < N; i++) {	
 	
 		double phi = atan2(P[i].e[1], P[i].e[0]);
