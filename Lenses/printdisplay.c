44cedbed30e7ed3f8da8988a6983d299ead4e565
AntonLueders <145451222+AntonLueders@users.noreply.github.com>
2026-07-13 23:13:00 +0200

    Update printdisplay.c

--- Lenses/printdisplay.c
+++ Lenses/printdisplay.c
@@ -43,13 +43,13 @@ void PrintWelcome() {
     printf("+                                                                  "
            "    "
            "      +\n");
-    printf("+             Last edited: 27.08.2025                              "
+    printf("+             Last edited: 13.07.2026                              "
            "    "
            "      +\n");
     printf("+                                                                  "
            "    "
            "      +\n");
-    printf("+             V1.0.1                                               "
+    printf("+             V1.0.2                                               "
            "          +\n");
     printf("+                                                                  "
            "    "
